55e8cdc5948c072c4261ce6ce724b16092f53a1b
Joe W <whobbes22@gmail.com>
2023-06-30 09:27:10 -0700

    add coffee item

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -5,20 +5,22 @@ function Coffee(props){
   return(
     <React.Fragment>
       <div onClick = {() => props.whenCoffeeClicked(props.id)}>
-        <h3>{props.species}</h3>
-        <p>{props.date}</p>
-        <p>{props.location}</p>
+        <h3>{props.name}</h3>
+        <p>{props.origin}</p>
+        <p>{props.price}</p>
+        <p>{props.roast}</p>
       </div>
     </React.Fragment>
   );
 }
 
 Coffee.propTypes = {
-  species: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  origin: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  roast: PropTypes.string.isRequired,
   id: PropTypes.string,
-  whenWhaleClicked: PropTypes.func
+  whenCoffeeClicked: PropTypes.func
 };
 
-export default Whale;
+export default Coffee;
