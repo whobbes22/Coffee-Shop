020a2b42d304166538cbefae9c1ee9e5a7348289
Joe W <whobbes22@gmail.com>
2023-06-30 08:14:21 -0700

    initialize files

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <div class="container">
+      <Header />
+      
+    </div>
+  </React.Fragment>
+  );
+}
+
+export default App;
